c46612c24ae92e20ca61f2edd387b3cb5b8668e4
matheusfelic <mbdf@cin.ufpe.br>
2016-06-13 23:42:06 -0300

    testing main

--- Main.js
+++ Main.js
@@ -1,6 +1,40 @@
-var lista1 = [1,2,3];
-var lista2 = [4,5,6];
+function lenAux(x,y)
+{
+    var k = [];
+    var w = (x == k);
+    if(w){
+        return y;
+    }else{
+        return lenAux(x.tail, y+1);
+    }
+}
 
-var x = lista1.head;
-var y = lista2.tail;
-var z = lista1.concat(lista2);
+function len (x){
+    return lenAux(x,0);
+}
+
+function quicksort(l){
+	var list = l.tail;
+	var menor = [];
+	var maior = [];
+	if(l == []){
+		return [];
+	}else{
+		while (list != []) {
+			if(l.head <= list.head){
+				menor.concat(list.head);
+			}else{
+				maior.concat(list.head);
+			}
+			list = list.tail;
+		}
+		var resultMaior = l.head
+		var result = quicksort(menor).concat([l.head].concat(quicksort(maior)));
+		return result;
+	}
+}
+
+var lista1 = [1,2,3];
+var c = len(lista1);
+var lista2 = [12,1,4,9,32,22,55,1,9,0,5]
+var z = quicksort(lista2);
